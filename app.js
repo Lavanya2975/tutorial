0d9e41dc621f5c4c97681483d24d8f0da3612a13
Lavanya2975 <168268031+Lavanya2975@users.noreply.github.com>
2024-06-06 17:53:26 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,245 +1,134 @@
 const express = require('express')
+const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
-const path = require('path')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const jwttoken = require('jsonwebtoken')
 
-const databasePath = path.join(__dirname, 'covid19IndiaPortal.db')
+let db = null
 
 const app = express()
 
 app.use(express.json())
+const dbpath = path.join(__dirname, 'covid19IndiaPortal.db')
 
-let db = null
-
-const initializeDbAndServer = async () => {
+const connection = async () => {
   try {
-    database = await open({
-      filename: databasePath,
-      driver: sqlite3.Database,
+    db = await open({filename: dbpath, driver: sqlite3.Database})
+    app.listen(3000, () => {
+      console.log('Server is running')
     })
-    app.listen(3000, () =>
-      console.log('Server Running at http://localhost:3000/'),
-    )
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`)
+  } catch (e) {
+    console.log('connection error : ' + e)
     process.exit(1)
   }
 }
 
-initializeDbAndServer()
+connection()
 
-const convertDbObjectToResponseObjectDis = dbObject => {
-  return {
-    districtId: dbObject.district_id,
-    districtName: dbObject.district_name,
-    stateId: dbObject.state_id,
-    cases: dbObject.cases,
-    cured: dbObject.cured,
-    active: dbObject.active,
-    deaths: dbObject.deaths,
+const check = (req, res, next) => {
+  let jwt
+  const header = req.headers['authorization']
+  if (header != undefined) {
+    jwt = header.split(' ')[1]
   }
-}
-const convertDbObjectToResponseObjectstats = dbObject => {
-  return {
-    totalCases: dbObject.cases,
-    totalCured: dbObject.cured,
-    totalActive: dbObject.active,
-    totalDeaths: dbObject.deaths,
-  }
-}
-
-const convertDbObjectToResponseObjectState = dbObject => {
-  return {
-    stateId: dbObject.state_id,
-    stateName: dbObject.state_name,
-    population: dbObject.population,
-  }
-}
-
-app.post('/register', async (request, response) => {
-  const {username, name, password, gender, location} = request.body
-  const hashedPassword = await bcrypt.hash(request.body.password, 10)
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`
-  const dbUser = await db.get(selectUserQuery)
-  if (dbUser !== undefined) {
-    response.status(400)
-    response.send('User already exists')
+  if (jwt === undefined) {
+    res.status(401)
+    res.send('Invalid JWT Token')
   } else {
-    if (password.length < 5) {
-      response.status(400)
-      response.send('Password is too short')
-    } else {
-      const createUserQuery = `
-      INSERT INTO 
-        user (username, name, password, gender, location) 
-      VALUES 
-        (
-          '${username}', 
-          '${name}',
-          '${hashedPassword}', 
-          '${gender}',
-          '${location}'
-        )`
-      const dbResponse = await db.run(createUserQuery)
-      response.status(200)
-      response.send('User created successfully')
-    }
-  }
-})
-
-app.post('/login', async (request, response) => {
-  const {username, password} = request.body
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`
-  const dbUser = await db.get(selectUserQuery)
-  if (dbUser !== undefined) {
-    const isPasswordMatched = await bcrypt.compare(
-      request.body.password,
-      dbUser.password,
-    )
-    if (isPasswordMatched === true) {
-      const payload = {
-        username: username,
-      }
-      const jwtToken = jwt.sign(payload, 'SECRET_TOKEN')
-      response.send({jwtToken})
-    } else {
-      response.status(400)
-      response.send('Invalid password')
-    }
-  } else {
-    response.status(400)
-    response.send('Invalid user')
-  }
-})
-
-const authenticateToken = (request, response, next) => {
-  let jwtToken
-  const authHeader = request.headers['authorization']
-  if (authHeader !== undefined) {
-    jwtToken = authHeader.split(' ')[1]
-  }
-  if (jwtToken === undefined) {
-    response.status(401)
-    response.send('Invalid JWT Token')
-  } else {
-    jwt.verify(jwtToken, 'SECRET_TOKEN', async (error, payload) => {
-      if (error) {
-        response.status(401)
-        response.send('Invalid JWT Token')
+    jwttoken.verify(jwt, 'secretkey', async (err, payload) => {
+      if (err) {
+        res.send('Invalid JWT Token')
       } else {
+        //console.log('correct jwt token')
         next()
       }
     })
   }
 }
 
-app.get('/states/', authenticateToken, async (request, response) => {
-  const Query = `
-    SELECT
-      *
-    FROM
-      state;`
-  const Array = await database.all(Query)
-  response.send(Array.map(each => convertDbObjectToResponseObjectState(each)))
+app.post('/login/', async (req, res) => {
+  const {username, password} = req.body
+
+  try {
+    const api1 = `SELECT * FROM user WHERE username = '${username}';`
+    const user = await db.get(api1)
+
+    if (user === undefined) {
+      res.status(400)
+      res.send('Invalid user')
+    } else {
+      const ispasswordright = await bcrypt.compare(password, user.password)
+      if (!ispasswordright) {
+        res.status(400)
+        res.send('Invalid password')
+      } else {
+        const payload = {
+          username: username,
+        }
+        const token = jwttoken.sign(payload, 'secretkey')
+        res.send({token})
+      }
+    }
+  } catch (e) {
+    console.log('Internal error : ' + e)
+  }
 })
 
-app.get('/states/:stateId/', authenticateToken, async (request, response) => {
-  const {stateId} = request.params
-  const Query = `
-    SELECT 
-      * 
-    FROM 
-      state 
-    WHERE 
-      state_id = ${stateId};`
-  const re = await database.get(Query)
-  response.send(convertDbObjectToResponseObjectState(re))
+app.get('/states', check, async (req, res) => {
+  try {
+    const api2 = `SELECT * FROM state;`
+    const ans = await db.all(api2)
+    res.send(ans)
+  } catch (e) {
+    console.log('get api error : ' + e)
+  }
 })
 
-app.post('/districts/', authenticateToken, async (request, response) => {
-  const {districtName, stateId, cases, cured, active, deaths} = request.body
-  const Query = `
-  INSERT INTO
-    district (district_name, state_id, cases, cured, active, deaths)
-  VALUES
-    ('${districtName}', ${stateId}, '${cases}', '${cured}', '${active}', '${deaths}');`
-  const re = await database.run(Query)
-  response.send('District Successfully Added')
+app.get('/states/:stateId', check, async (req, res) => {
+  const {stateId} = req.params
+  const api3 = `SELECT * FROM state WHERE state_id='${stateId}';`
+  const ans = await db.get(api3)
+  res.send(ans)
 })
 
-app.get(
-  '/districts/:districtId/',
-  authenticateToken,
-  async (request, response) => {
-    const {districtId} = request.params
-    const Query = `
-    SELECT 
-      * 
-    FROM 
-      district 
-    WHERE 
-      district_id = ${districtId};`
-    const re = await database.get(Query)
-    response.send(convertDbObjectToResponseObjectDis(re))
-  },
-)
+app.post('/districts/', check, async (req, res) => {
+  const {districtName, stateId, cases, cured, active, deaths} = req.body
+  const api4 = `INSERT INTO
+                    district ( district_name, state_id, cases,cured,active,deaths)
+                VALUES
+                    ('${districtName}','${stateId}','${cases}','${cured}','${active}','${deaths}');`
+  await db.run(api4)
+  res.send('District Successfully Added')
+})
 
-app.delete(
-  '/districts/:districtId/',
-  authenticateToken,
-  async (request, response) => {
-    const {districtId} = request.params
-    const Query = `
-  DELETE FROM
-    district
-  WHERE
-    district_id = ${districtId};`
-    await database.run(Query)
-    response.send('District Removed')
-  },
-)
+app.get('/districts/:districtId/', check, async (req, res) => {
+  const {districtId} = req.params
+  const api5 = `SELECT * FROM district WHERE district_id = '${districtId}';`
+  const ans = await db.get(api5)
+  res.send(ans)
+})
 
-app.put(
-  '/districts/:districtId/',
-  authenticateToken,
-  async (request, response) => {
-    const {districtName, stateId, cases, cured, active, deaths} = request.body
-    const {districtId} = request.params
-    const Query = `
-  UPDATE
-    district
-  SET
-    district_name = '${districtName}',
-    state_id = ${stateId}, 
-    cases = '${cases}', 
-    cured = '${cured}', 
-    active = '${active}', 
-    deaths = '${deaths}'
-  WHERE
-    district_id = ${districtId};`
+app.delete('/districts/:districtId/', check, async (req, res) => {
+  const {districtId} = req.params
+  const api6 = `DELETE FROM district WHERE district_id = '${districtId}';`
+  await db.run(api6)
+  res.send('District Removed')
+})
 
-    await database.run(Query)
-    response.send('District Details Updated')
-  },
-)
+app.put('/districts/:districtId/', check, async (req, res) => {
+  const {districtId} = req.params
+  const {districtName, stateId, cases, cured, active, deaths} = req.body
+  const api7 = `UPDATE district SET district_name = '${districtName}',state_id = '${stateId}',cases = '${cases}',cured = '${cured}',active = '${active}',deaths = '${deaths}' WHERE district_id = '${districtId}';`
+  await db.run(api7)
+  res.send('District Details Updated')
+})
 
-app.get(
-  '/states/:stateId/stats/',
-  authenticateToken,
-  async (request, response) => {
-    const {stateId} = request.params
-    const Query = `
-    SELECT
-      SUM(cases) as cases, SUM(cured) as cured, SUM(active) as active, SUM(deaths) as deaths
-    FROM
-      district
-    WHERE state_id = ${stateId}
-    GROUP BY state_id;`
-    const re = await database.get(Query)
-    response.send(convertDbObjectToResponseObjectstats(re))
-  },
-)
+app.get('/states/:stateId/stats', check, async (req, res) => {
+  const {stateId} = req.params
+  const api8 = `SELECT SUM(cases) AS totalCases,SUM(cured) AS totalCured,SUM(active) AS totalActive,SUM(deaths) AS totalDeaths FROM district WHERE state_id = '${stateId}';`
+  const ans = await db.get(api8)
+  res.send(ans)
+})
 
 module.exports = app
